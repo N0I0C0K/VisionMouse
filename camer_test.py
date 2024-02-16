6323388df11d56d6df1c5cc965ac82172125abdb
N0I0C0K <nick131410@aliyun.com>
2024-02-16 16:22:05 +0800

    feat：添加了鼠标模拟

--- camer_test.py
+++ camer_test.py
@@ -2,15 +2,15 @@ import cv2
 import numpy as np
 import torch
 
+from queue import Queue
+
 from model.net import get_model, get_transforms, DEVICE, classes, IMG_SIZE
 
-from mediapipe.python.solutions import (
-    drawing_utils as mp_drawing,
-    drawing_styles as mp_drawing_styles,
-    hands as mhands,
-)
+from mediapipe.python.solutions import hands as mhands
 
 from filter import SlidingWindowMeanFilter
+
+import pyautogui
 
 model = get_model("checkpoints/RetinaNet_ResNet50.pth")
 transform = get_transforms()
@@ -22,6 +22,9 @@ def process_img(img: np.ndarray) -> tuple[tuple[int, int], torch.Tensor]:
     processed_image: torch.Tensor = transformed_image["image"] / 255.0
 
     return ((width, height), processed_image.to(DEVICE))
+
+
+pos_queue: Queue[tuple[int, int]] = Queue(1)
 
 
 def raw_model():
@@ -37,7 +40,7 @@ def raw_model():
     )
 
     filters = {4: SlidingWindowMeanFilter(3), 8: SlidingWindowMeanFilter(3)}
-
+    last_pos = (0, 0)
     with torch.no_grad():
         while cap.isOpened():
             ret, frame = cap.read()
@@ -62,17 +65,20 @@ def raw_model():
             #             (boxes[i][3] - padding_h) * scale
             #         )
             #         cv2.rectangle(frame, pos1, pos2, (0, 255, 0), 2)
-
-            results = hands.process(frame[:, :, ::-1])
+            resized_frame = cv2.resize(frame, (width // 2, height // 2))
+            results = hands.process(resized_frame[:, :, ::-1])
 
             if results.multi_hand_landmarks:  # type: ignore
                 for hand_landmarks in results.multi_hand_landmarks:  # type: ignore
                     for idx, landmark in enumerate(hand_landmarks.landmark):
+                        x = int(landmark.x * frame.shape[1])
+                        y = int(landmark.y * frame.shape[0])
                         if idx == 8 or idx == 4:  # 食指指尖的关键点索引为8
-                            x = int(landmark.x * frame.shape[1])
-                            y = int(landmark.y * frame.shape[0])
                             # pos = filters[idx].push((x, y))
                             cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
+
+                            if idx == 8:
+                                pos_queue.put((x, y))
 
             cv2.imshow("test", frame)
             key = cv2.waitKey(1)
@@ -83,4 +89,24 @@ def raw_model():
     cv2.destroyAllWindows()
 
 
-raw_model()
+def mouse_handle():
+
+    last_pos = pos_queue.get()
+    while True:
+        # print(pos_queue.qsize())
+        pos = pos_queue.get()
+        dx = pos[0] - last_pos[0]
+        dy = pos[1] - last_pos[1]
+        pyautogui.move(dx, dy, duration=0.1)
+        last_pos = pos
+
+
+def main():
+    from concurrent.futures.thread import ThreadPoolExecutor
+
+    pool = ThreadPoolExecutor(2)
+    handler = pool.submit(mouse_handle)
+    raw_model()
+
+
+main()
